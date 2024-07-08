fe9662852d3537527177792537f35c342d3662f7
Siddharth Vyas <siddharth.vyas9299@gmail.com>
2024-07-08 22:08:57 +0530

    Added logic for Event URL

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,21 @@
 
 class Event < ActiveRecord::Base
 	belongs_to :user
+
+  def event_url
+    Rails.application.routes.url_helpers.event_url(self, host: host_for_env)
+  end
+
+  private
+
+  def host_for_env
+    case Rails.env
+    when "development"
+      "localhost:3000"
+    when "production"
+      "example.net"
+    else
+      "example.com"
+    end
+  end
 end
