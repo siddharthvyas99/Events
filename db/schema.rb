5809e5739a10043ac9f9d17ad6d7f4bfb0b1c32c
Siddharth Vyas <siddharth.vyas9299@gmail.com>
2024-07-08 18:41:47 +0530

    Added Events table with migration    - Added required fields with users association

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_08_123536) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_08_130609) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "events", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "start_datetime"
+    t.datetime "end_datetime"
+    t.float "entry_fee"
+    t.string "status"
+    t.uuid "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
 
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,4 +45,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_08_123536) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "users"
 end
